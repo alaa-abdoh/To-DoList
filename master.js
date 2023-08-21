4180f3aa0e0d3d9811f89fdd1c2720b95942ae9a
alaa-abdoh <alaajabdoh1701@gmail.com>
2023-08-21 12:00:42 +0300

    add done feature to switch the task from status to another

--- master.js
+++ master.js
@@ -10,17 +10,17 @@ async function fetchData (){
 
 fetchData().then(() => {
     isLoading = false;
-    fillTable()
+    fillTable(data)
 })
 
-function fillTable(){
+function fillTable(givenData){
     let loader = document.querySelector(".loading");
     if(isLoading)
     null
 else 
    { loader.style.display = "none";
      let tbody = document.querySelector("table tbody")
-    data.forEach((obj)=>{
+     givenData.forEach((obj)=>{
         // console.log(obj)
         let id = obj.id;
         let description = obj.todo;
@@ -36,15 +36,39 @@ else
         }
         let deleteBtn = document.createElement("button");
         deleteBtn.id = "delete"
+        deleteBtn.dataset.id = obj.id;
         deleteBtn.appendChild(document.createTextNode("Delete"))
         let doneBtn = document.createElement("button");
         doneBtn.id = "done"
+        doneBtn.dataset.id = obj.id;
         doneBtn.appendChild(document.createTextNode("Done"))
         let td = document.createElement("td");
         td.append(deleteBtn, doneBtn)
         tableRow.appendChild(td)
+        obj.completed ? tableRow.style.background = "#cccccc3d": null
         tbody.appendChild(tableRow)
     })
     let count = document.querySelector("table tfoot span")
-    count.innerHTML = data.length;}
+    count.innerHTML = givenData.length;}
 }
+
+//**************************************************
+function clearTable(){
+    let tbody = document.querySelector("table tbody")
+    tbody.innerHTML = ""
+}
+function done(){
+    document.addEventListener("click",(e)=>{
+        if(e.target.id == "done"){
+            let rowId = e.target.dataset.id;
+            data.forEach((obj)=>{
+                if (obj.id == rowId){
+                    obj.completed ? obj.completed = false : obj.completed = true
+                }
+            })
+            clearTable();
+            fillTable(data)
+        }
+    })
+}
+done()
